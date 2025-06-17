cbabeb89d99f2aa8304260d832c4bfd9dd6a8a67
codeNow26 <code.now26@gmail.com>
2025-06-17 11:17:51 +0200

    implemented stats for every Pokemo and  arrow functions where u can select next and previous Pokemon

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ async function init() {
     await getPokemon();
 }
 
+
+let allPokemon = {};
+let pkmStats = {};
+
 const Base_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 let typeColor = {
     grass: "green",
     water: "blue",
     poison: "purple",
-    bug: "light-blue",
+    bug: "dark-green",
     flying: "white",
     fire: "red",
 }
-
-
 
 async function fetchPokemon(i) {
     let response = await fetch(Base_URL + i);
@@ -33,21 +35,27 @@ async function fetchPokemon(i) {
     };
 }
 
-function showPokemon(i, pkmData) {
-    let pkmCard = document.getElementById(`card-${i}`)
-    let secondType = "";
+async function fetchPokemonStats(i) {
+    let response = await fetch(Base_URL + i);
+    let data = await response.json();
+    let pkmStats = [];
+    for (let t = 0; t < data.stats.length; t++) {
+        pkmStats.push(data.stats[t].stat.name);
+        pkmStats.push(data.stats[t].base_stat);
+    }
 
-    if (pkmData.pkmtypes[1]) {
-        secondType = `<div id="pkm-type2-${i}" class="type">${pkmData.pkmtypes[1]}</div>`
-    };
+    return data.stats;
+}
 
-    pkmCard.innerHTML = `
-   <div class="pkm-name">${pkmData.name}</div>
-   <img src="${pkmData.img}">
-     <div class="types-wrapper">
-    <div id="pkm-type-${i}" class="type">${pkmData.pkmtypes[0]}</div>
-    ${secondType}`
-    pokemonTypeColor(i, pkmData);
+
+
+function openDialogWindow(i, event) {
+    bubblingPrevention(event);
+    currentIndex = i;
+    let pkmData = allPokemon[i];
+    let pkmStatsData = pkmStats[i];
+    showPokemonOverlay(i, pkmData, pkmStatsData);
+    document.getElementById('dialog').style.display = "block";
 }
 
 function pokemonTypeColor(i, pkmData) {
@@ -63,82 +71,64 @@ function pokemonTypeColor(i, pkmData) {
     }
 }
 
-let allPokemon = {};
-
 async function getPokemon() {
     for (let i = 1; i < 16; i++) {
         let pkmData = await fetchPokemon(i);
+        let pkmStatsData = await fetchPokemonStats(i);
+        pkmStats[i] = pkmStatsData;
         allPokemon[i] = pkmData;
         showPokemon(i, pkmData);
+
     }
 }
 
-function createCard() {
-    let pokemon = document.getElementById('pokemon-container');
-    for (let i = 1; i < 16; i++) {
-        pokemon.innerHTML += `
-        <div onclick="openDialogWindow(${i})" class="card" id="card-${i}">
-        </div>`
-    }
+function nextPokemon() {
+    currentIndex++;
+    if (currentIndex > 15) currentIndex = 1;
+
+    let pkmStatsData = pkmStats[currentIndex]
+    let pkmData = allPokemon[currentIndex];
+    showPokemonOverlay(currentIndex, pkmData, pkmStatsData);
 }
 
-function openDialogWindow(i) {
-      let pkmData = allPokemon[i];
-    showPokemonOverlay(i, pkmData);
+function previousPokemon() {
+    currentIndex--;
+    if (currentIndex === 0) currentIndex = 15;
+
+    let pkmStatsData = pkmStats[currentIndex]
+    let pkmData = allPokemon[currentIndex];
+    showPokemonOverlay(currentIndex, pkmData, pkmStatsData);
+}
+
+function bubblingPrevention(event) {
+    event.stopPropagation()
     document.getElementById('dialog').style.display = "block";
 }
 
-function showPokemonOverlay(i, pkmData) {
-    
-    let secondType = "";
-    let showPokemonDialog = document.getElementById('dialog');
-
-    if (pkmData.pkmtypes[1]) {
-        secondType = `<div id="pkm-type-${i}" class="type big-type ${typeColor[pkmData.pkmtypes[1]] || ''}">${pkmData.pkmtypes[1]}</div>`
-    };
-
-    showPokemonDialog.innerHTML = `
-        <div class="big-pkm-wrapper" id="dialog-${i}">
-           <div class="card bigCard" id="card-${i}">
-           <div class="head-pkm-card">
-         <img class="pokeball" src="./img/Pokeball.png">
-         <img class="bigPkmImage" src="${pkmData.img}">
-        <div class="big-pkm-name">${pkmData.name}</div>
-        <div class="big-types-wrapper">
-     <div id="pkm-type-${i}" class="type big-type ${typeColor[pkmData.pkmtypes[0]] || ''}">${pkmData.pkmtypes[0]}</div>
-    ${secondType}
-          </div>
-          </div>
-           </div>`
-               + `${showArrows()}`
-           pokemonTypeColor(i, pkmData);
+function closeDialog() {
+    let hidden = document.getElementById('dialog');
+    hidden.style.display = "none"
 }
 
+function statsBar(i, pkmStatsData) {
+    let bar1 = document.getElementById(`atkBar-${i}`);
+    let bar2 = document.getElementById(`dfnsBar-${i}`);
+    let bar3 = document.getElementById(`spclAtkBar-${i}`);
+    let bar4 = document.getElementById(`spclDfnsBar-${i}`);
+    let bar5 = document.getElementById(`speedBar-${i}`);
 
-function showArrows() {
-    return `<div class="arrow-wrapper">
-         <img id="left-arrow" class="arrow" src="./img/arrow left.png">
-        <img onclick='nextPokemon()' id="right-arrow" class="arrow" src="./img/arrow right.png">
-           </div>
-            </div>
-           `
+    bar1.style.width = pkmStatsData[1].base_stat + "%";
+    bar1.textContent =  + pkmStatsData[1].base_stat;
+
+    bar2.style.width = pkmStatsData[2].base_stat + "%";
+    bar2.textContent = pkmStatsData[2].base_stat;
+
+    bar3.style.width = pkmStatsData[3].base_stat + "%";
+    bar3.textContent = pkmStatsData[3].base_stat;
+
+    bar4.style.width = pkmStatsData[4].base_stat + "%";
+    bar4.textContent = pkmStatsData[4].base_stat;
+
+    bar5.style.width = pkmStatsData[5].base_stat + "%";
+    bar5.textContent = pkmStatsData[5].base_stat;
 }
-
-function nextPokemon(i, pkmData) {
-    for (let i = 0; i < 16; i++) {
-        showPokemonDialog.innerHTML = 
-        showPokemonOverlay(i);
-    }
-}
-
-console.log(showPokemonOverlay(1));
-
-
-function previousPokemon() {
-    
-}
-
-function closeOverlay() {
-    document.getElementById('dialog').style.display = "none"
-}
-
---
+++ templates.js
@@ -0,0 +1,88 @@
+function createCard() {
+    let pokemon = document.getElementById('pokemon-container');
+    for (let i = 1; i < 16; i++) {
+        pokemon.innerHTML += `
+        <div onclick="openDialogWindow(${i}, event)" class="card" id="card-${i}">
+        </div>`
+    }
+}
+
+function showPokemon(i, pkmData) {
+    let pkmCard = document.getElementById(`card-${i}`)
+    let secondType = "";
+
+    if (pkmData.pkmtypes[1]) {
+        secondType = `<div id="pkm-type2-${i}" class="type">${pkmData.pkmtypes[1]}</div>`
+    };
+
+    pkmCard.innerHTML = `
+   <div class="pkm-name">${pkmData.name}</div>
+   <img src="${pkmData.img}">
+     <div class="types-wrapper">
+    <div id="pkm-type-${i}" class="type">${pkmData.pkmtypes[0]}</div>
+    ${secondType}`
+    pokemonTypeColor(i, pkmData);
+}
+
+
+
+function showPokemonOverlay(i, pkmData, pkmStatsData) {
+
+    let secondType = "";
+    let showPokemonDialog = document.getElementById('dialog');
+
+    if (pkmData.pkmtypes[1]) {
+        secondType = `<div id="pkm-type-${i}" class="type big-type ${typeColor[pkmData.pkmtypes[1]] || ''}">${pkmData.pkmtypes[1]}</div>`
+    };
+
+    showPokemonDialog.innerHTML = `
+        <div class="big-pkm-wrapper" id="dialog-${i}">
+           <div class="card bigCard" id="card-${i}">
+           <div class="head-pkm-card">
+         <img class="pokeball" src="./img/Pokeball.png">
+         <img class="bigPkmImage" src="${pkmData.img}">
+        <div class="big-pkm-name">${pkmData.name}</div>
+        <div class="big-types-wrapper">
+     <div id="pkm-type-${i}" class="type big-type ${typeColor[pkmData.pkmtypes[0]] || ''}">${pkmData.pkmtypes[0]}</div>
+    ${secondType}
+          </div>
+          </div>
+          
+<div class="progress-container">
+  <div class="progress-row">
+    <span class="stat-name">${pkmStatsData[1].stat.name}:</span>
+    <div class="progress-bar" id="atkBar-${i}">0%</div>
+  </div>
+  <div class="progress-row">
+    <span class="stat-name">${pkmStatsData[2].stat.name}:</span>
+    <div class="progress-bar" id="dfnsBar-${i}">0%</div>
+  </div>
+  <div class="progress-row">
+    <span class="stat-name">${pkmStatsData[3].stat.name}:</span>
+    <div class="progress-bar" id="spclAtkBar-${i}">0%</div>
+  </div>
+  <div class="progress-row">
+    <span class="stat-name">${pkmStatsData[4].stat.name}:</span>
+    <div class="progress-bar" id="spclDfnsBar-${i}">0%</div>
+  </div>
+  <div class="progress-row">
+    <span class="stat-name">${pkmStatsData[5].stat.name}:</span>
+    <div class="progress-bar" id="speedBar-${i}">0%</div>
+  </div>
+</div>`
+
+    pokemonTypeColor(i, pkmData);
+    statsBar(i, pkmStatsData);
+            showArrows();
+}
+
+
+function showArrows() {
+    let arrows = document.getElementById('dialog');
+    arrows.innerHTML +=  `<div class="arrow-wrapper">
+         <img onclick ='previousPokemon()' id="left-arrow" class="arrow" src="./img/arrow left.png">
+        <img onclick='nextPokemon()' id="right-arrow" class="arrow" src="./img/arrow right.png">
+           </div>
+            </div>
+           `
+}
