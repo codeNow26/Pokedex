e0951977fd0cee83ec507cca02c7807dba008e38
codeNow26 <code.now26@gmail.com>
2025-07-02 10:02:27 +0200

    Responsive improvements, function cleanup, bug fixes

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
-async function init() {
-    createCard(1, 25);
-    await getPokemon(1, currentPokemon);
-}
-
 let allPokemon = {};
 let pkmStats = {};
 let currentPokemon = 25;
+let lastInputLength = 0;
 const Base_URL = "https://pokeapi.co/api/v2/pokemon/";
 
-let typeColor = {
-    grass: "green",
-    water: "blue",
-    poison: "purple",
-    bug: "dark-green",
-    flying: "white",
-    fire: "red",
-    normal: "indianred",
-    electric: "yellow",
-    ground: "brown",
-    fairy: "pink",
-    fighting: "orange",
-    psychic: "magenta",
-    ice: "light-blue",
-    rock: "light-brown",
-    steel: "light-green",
-    dragon: "gold",
-    dark: "darkgray"
+
+
+async function init() {
+    document.getElementById("search-bar").addEventListener("input", searchPokemon);
+    createCards(1, 25);
+    await getPokemon(1, currentPokemon);
 }
 
 async function fetchPokemon(i) {
-    let response = await fetch(Base_URL + i);
-    let data = await response.json();
+    const response = await fetch(Base_URL + i);
+    const data = await response.json();
     let pkmtypes = [];
     for (let t = 0; t < data.types.length; t++) {
         pkmtypes.push(data.types[t].type.name);
     }
     let name = data.name;
-    let img = data.sprites.other.dream_world.front_default
-
+    let img = data.sprites.other.dream_world.front_default;
     return {
         name,
         pkmtypes,
@@ -51,30 +34,41 @@ async function fetchPokemonStats(i) {
     return data.stats;
 }
 
-function openDialogWindow(i, event) {
-    bubblingPrevention(event);
+function openDialogWindow(i) {
     currentIndex = i;
     let pkmData = allPokemon[i];
     let pkmStatsData = pkmStats[i];
     showPokemonOverlay(i, pkmData, pkmStatsData);
     document.getElementById('dialog').style.display = "block";
     document.body.style.overflow = "hidden";
+    toggleArrows();
+}
+
+function toggleArrows() {
+    let arrows = document.getElementById('arrows');
+    let searchInputLength = document.getElementById("search-bar").value.trim().length;
+    if (arrows) {
+        if (searchInputLength >= 3) {
+            arrows.style.display = "none";
+        } else {
+            arrows.style.display = "flex";
+        }
+    }
 }
 
 function pokemonTypeColor(i, pkmData) {
-    let addColor = document.getElementById(`pkm-type-${i}`);
-    let addColor2 = document.getElementById(`pkm-type2-${i}`);
+    let addPrimaryColor = document.getElementById(`pkm-type-${i}`);
+    let addSecondaryColor = document.getElementById(`pkm-type2-${i}`);
     let addColorToBackground = document.getElementById(`card-${i}`);
     let firstType = pkmData.pkmtypes[0];
     let secondaryType = pkmData.pkmtypes[1];
-    if (typeColor[firstType]) {
-        addColor.classList.add(typeColor[firstType]);
+
+    if (firstType) {
+       addPrimaryColor.classList.add(firstType);
+       addColorToBackground.classList.add(firstType);
     }
-    if (typeColor[secondaryType]) {
-        addColor2.classList.add(typeColor[secondaryType]);
-    }
-    if (typeColor[firstType]) {
-        addColorToBackground.classList.add(typeColor[firstType]);
+     if (secondaryType) {
+        addSecondaryColor.classList.add(secondaryType);
     }
 }
 
@@ -86,6 +80,28 @@ async function getPokemon(start, end) {
         allPokemon[i] = pkmData;
         showPokemon(i, pkmData);
     }
+}
+
+function createCards(start, end) {
+    let pkmContainer = document.getElementById('pokemon-container');
+    for (let i = start; i <= end; i++) {
+        pkmContainer.innerHTML += createCardsTemplate(i);
+    }
+}
+
+function showPokemon(i, pkmData) {
+    let pkmCard = document.getElementById(`card-${i}`);
+    pkmCard.innerHTML = showPokemonTemplate(i, pkmData);
+    pokemonTypeColor(i, pkmData);
+}
+
+function showPokemonOverlay(i, pkmData, pkmStatsData) {
+    let showPokemonDialog = document.getElementById('dialog');
+    showPokemonDialog.innerHTML = showPokemonOverlayTemplate(i, pkmData, pkmStatsData);
+
+    pokemonTypeColor(i, pkmData);
+    statsBar(i, pkmStatsData);
+    showArrows();
 }
 
 function nextPokemon() {
@@ -106,37 +122,32 @@ function previousPokemon() {
     showPokemonOverlay(currentIndex, pkmData, pkmStatsData);
 }
 
-function bubblingPrevention(event) {
-    event.stopPropagation()
-    document.getElementById('dialog').style.display = "block";
-}
-
 function closeDialog() {
     let hidden = document.getElementById('dialog');
     hidden.style.display = "none"
-    document.body.style.overflow = "scroll";
+    document.body.style.overflow = "auto";
 }
 
 function statsBar(i, pkmStatsData) {
-    let bar0 = document.getElementById(`hpBar-${i}`);
-    let bar1 = document.getElementById(`atkBar-${i}`);
-    let bar2 = document.getElementById(`dfnsBar-${i}`);
-    let bar3 = document.getElementById(`spclAtkBar-${i}`);
-    let bar4 = document.getElementById(`spclDfnsBar-${i}`);
-    let bar5 = document.getElementById(`speedBar-${i}`);
+    let hpBar = document.getElementById(`hpBar-${i}`);
+    let atkBar = document.getElementById(`atkBar-${i}`);
+    let dfnsBar = document.getElementById(`dfnsBar-${i}`);
+    let spclAtkBar = document.getElementById(`spclAtkBar-${i}`);
+    let spclDfnsBar = document.getElementById(`spclDfnsBar-${i}`);
+    let speedBar = document.getElementById(`speedBar-${i}`);
 
-    bar0.style.width = pkmStatsData[1].base_stat + "%";
-    bar0.textContent = + pkmStatsData[1].base_stat;
-    bar1.style.width = pkmStatsData[1].base_stat + "%";
-    bar1.textContent = + pkmStatsData[1].base_stat;
-    bar2.style.width = pkmStatsData[2].base_stat + "%";
-    bar2.textContent = pkmStatsData[2].base_stat;
-    bar3.style.width = pkmStatsData[3].base_stat + "%";
-    bar3.textContent = pkmStatsData[3].base_stat;
-    bar4.style.width = pkmStatsData[4].base_stat + "%";
-    bar4.textContent = pkmStatsData[4].base_stat;
-    bar5.style.width = pkmStatsData[5].base_stat + "%";
-    bar5.textContent = pkmStatsData[5].base_stat;
+    hpBar.style.width = pkmStatsData[1].base_stat + "%";
+    hpBar.textContent = + pkmStatsData[1].base_stat;
+    atkBar.style.width = pkmStatsData[1].base_stat + "%";
+    atkBar.textContent = + pkmStatsData[1].base_stat;
+    dfnsBar.style.width = pkmStatsData[2].base_stat + "%";
+    dfnsBar.textContent = pkmStatsData[2].base_stat;
+    spclAtkBar.style.width = pkmStatsData[3].base_stat + "%";
+    spclAtkBar.textContent = pkmStatsData[3].base_stat;
+    spclDfnsBar.style.width = pkmStatsData[4].base_stat + "%";
+    spclDfnsBar.textContent = pkmStatsData[4].base_stat;
+    speedBar.style.width = pkmStatsData[5].base_stat + "%";
+    speedBar.textContent = pkmStatsData[5].base_stat;
 }
 
 async function loadNewPokemon() {
@@ -145,7 +156,7 @@ async function loadNewPokemon() {
     currentPokemon = end;
 
     showLoading();
-    createCard(start, end);
+    createCards(start, end);
     await getPokemon(start, end);
     hideLoading();
 }
@@ -158,26 +169,37 @@ function hideLoading() {
     document.getElementById('loading-overlay').classList.add('hidden');
 }
 
-let lastInputLength = 0;
-
 function searchPokemon() {
-    let input = document.getElementById("search-bar").value.trim().toLowerCase();
-    let pokemon = document.getElementById('pokemon-container');
+    let searchInput = document.getElementById("search-bar").value.trim().toLowerCase();
     let loadBtn = document.getElementById('load-new-pokemon');
-
-    if (nameInput(input.length, loadBtn, pokemon)) return;
-
-    lastInputLength = input.length;
-    pokemon.innerHTML = "";
-    filterPokemon(input, pokemon, loadBtn);
+    let pkmContainer = document.getElementById('pokemon-container');
+    pkmContainer.classList.toggle('full-width', searchInput.length >= 3);
+    if (searchInput.length >= 3) hideArrowsAndLoadButton(searchInput, pkmContainer, loadBtn);
+    if (nameInput(searchInput.length, loadBtn, pkmContainer)) return;
+    lastInputLength = searchInput.length;
+    pkmContainer.innerHTML = "";
+    filterPokemon(searchInput, pkmContainer, loadBtn);
 }
 
-function nameInput(length, loadBtn, pokemon) {
+function hideArrowsAndLoadButton(searchInput, pkmContainer, loadBtn) {
+    let arrows = document.getElementById('arrows')
+    if (searchInput.length >= 3 && pkmContainer && arrows) {
+        arrows.style.display = "none";
+    }
+    if (searchInput.length >= 3 && pkmContainer) {
+        loadBtn.style.display = "none";
+        showReturnBtn();
+    }
+
+}
+
+function nameInput(length, loadBtn, pkmContainer) {
     if (length < 3) {
         loadBtn.style.display = "block";
+        hideReturnBtn();
         if (lastInputLength >= 3) {
-            pokemon.innerHTML = "";
-            createCard(1, 25);
+            pkmContainer.innerHTML = "";
+            createCards(1, 25);
             getPokemon(1, currentPokemon);
         }
         lastInputLength = length;
@@ -186,19 +208,28 @@ function nameInput(length, loadBtn, pokemon) {
     return false;
 }
 
-function filterPokemon(input, pokemon, loadBtn) {
+function filterPokemon(input, pkmContainer, loadBtn) {
     let found = false;
+    pkmContainer.innerHTML = "";
     for (let i in allPokemon) {
         let pkm = allPokemon[i];
         if (pkm && pkm.name.toLowerCase().includes(input)) {
-            pokemon.innerHTML += `
-                <div onclick="openDialogWindow(${i}, event)" class="card" id="card-${i}"></div>`;
-            showPokemon(i, pkm);
-            found = true;
+            pkmContainer.innerHTML += createPokemonCardTemplate(i);
+            showPokemon(i, pkm); found = true;
         }
-    }
-    if (!found) {
-        pokemon.innerHTML = `<p class="no-results">No Pokémon found matching</p>`;
+    } if (!found) {
+        pkmContainer.innerHTML += showNoResults(), showReturnBtn();
         loadBtn.style.display = "none";
     }
+}
+
+function showReturnBtn() {
+    let btn = document.getElementById('return-btn');
+    if (btn) {
+        btn.style.display = "block";
+    }
+}
+
+function hideReturnBtn() {
+    document.getElementById('return-btn').style.display = "none";
 }
--- templates.js
+++ templates.js
@@ -1,21 +1,17 @@
-function createCard(start, end) {
-  let pokemon = document.getElementById('pokemon-container');
-  for (let i = start; i <= end; i++) {
-    pokemon.innerHTML += `
+function createCardsTemplate(i) {
+  return `
         <div onclick="openDialogWindow(${i}, event)" class="card" id="card-${i}">
-        </div>`
-  }
+        <div class="dialog-content"></div>
+        </div>`;
 }
 
-function showPokemon(i, pkmData) {
-  let pkmCard = document.getElementById(`card-${i}`);
+function showPokemonTemplate(i, pkmData) {
   let secondType = "";
-
   if (pkmData.pkmtypes[1]) {
     secondType = `<div id="pkm-type2-${i}" class="type">${pkmData.pkmtypes[1]}</div>`
   };
 
-  pkmCard.innerHTML = `
+  return `
     <div class="pkm-card-wrapper">
        <div class="pkm-id">#${i}</div>
    <div class="pkm-name">${pkmData.name.toUpperCase()}</div>
@@ -26,69 +22,77 @@ function showPokemon(i, pkmData) {
     <div id="pkm-type-${i}" class="type">${pkmData.pkmtypes[0]}</div>
     ${secondType}
         </div>
-    </div>`
-
-  pokemonTypeColor(i, pkmData);
+    </div>`;
 }
 
-function showPokemonOverlay(i, pkmData, pkmStatsData) {
+function showPokemonOverlayTemplate(i, pkmData, pkmStatsData) {
   let secondType = "";
-  let showPokemonDialog = document.getElementById('dialog');
-
   if (pkmData.pkmtypes[1]) {
-    secondType = `<div id="pkm-type-${i}" class="type big-type ${typeColor[pkmData.pkmtypes[1]] || ''}">${pkmData.pkmtypes[1]}</div>`
-  };
-
-  showPokemonDialog.innerHTML = `
+    secondType = `<div id="pkm-type-${i}-2" class="type big-type ${pkmData.pkmtypes[1]}">${pkmData.pkmtypes[1]}</div>`;
+  }
+  return `
         <div class="big-pkm-wrapper" id="dialog-${i}">
-           <div class="card bigCard" id="card-${i}">
-           <div class="head-pkm-card">
-         <img class="pokeball" src="./img/Pokeball.png">
-         <img class="bigPkmImage" src="${pkmData.img}">
-        <div class="big-pkm-name">${pkmData.name}</div>
-        <div class="big-types-wrapper">
-     <div id="pkm-type-${i}" class="type big-type ${typeColor[pkmData.pkmtypes[0]] || ''}">${pkmData.pkmtypes[0]}</div>
-    ${secondType}
+          <img onclick="closeDialog()" class="close-window-x" src="./img/x.png.png">
+          <div class="card bigCard" id="card-${i}">
+            <div class="head-pkm-card">
+              <img class="pokeball" src="./img/Pokeball.png">
+              <img class="bigPkmImage" src="${pkmData.img}">
+              <div class="big-pkm-name">${pkmData.name}</div>
+              <div class="big-types-wrapper">
+                <div id="pkm-type-${i}-1" class="type big-type ${pkmData.pkmtypes[0]}">${pkmData.pkmtypes[0]}</div>
+                ${secondType}
+              </div>
+            </div>
+            
+            <div id="progress" class="progress-container">
+              <div class="progress-row">
+                <span class="stat-name">${pkmStatsData[0].stat.name}:</span>
+                <div class="progress-bar" id="hpBar-${i}">0%</div>
+              </div>
+              <div class="progress-row">
+                <span class="stat-name">${pkmStatsData[1].stat.name}:</span>
+                <div class="progress-bar" id="atkBar-${i}">0%</div>
+              </div>
+              <div class="progress-row">
+                <span class="stat-name">${pkmStatsData[2].stat.name}:</span>
+                <div class="progress-bar" id="dfnsBar-${i}">0%</div>
+              </div>
+              <div class="progress-row">
+                <span class="stat-name">${pkmStatsData[3].stat.name}:</span>
+                <div class="progress-bar" id="spclAtkBar-${i}">0%</div>
+              </div>
+              <div class="progress-row">
+                <span class="stat-name">${pkmStatsData[4].stat.name}:</span>
+                <div class="progress-bar" id="spclDfnsBar-${i}">0%</div>
+              </div>
+              <div class="progress-row">
+                <span class="stat-name">${pkmStatsData[5].stat.name}:</span>
+                <div class="progress-bar" id="speedBar-${i}">0%</div>
+              </div>
+            </div>
           </div>
-          </div>
-          
-<div id ="progress" class="progress-container">
-  <div class="progress-row">
-    <span class="stat-name">${pkmStatsData[0].stat.name}:</span>
-    <div class="progress-bar" id="hpBar-${i}">0%</div>
-  </div>
-  <div class="progress-row">
-    <span class="stat-name">${pkmStatsData[1].stat.name}:</span>
-    <div class="progress-bar" id="atkBar-${i}">0%</div>
-  </div>
-  <div class="progress-row">
-    <span class="stat-name">${pkmStatsData[2].stat.name}:</span>
-    <div class="progress-bar" id="dfnsBar-${i}">0%</div>
-  </div>
-  <div class="progress-row">
-    <span class="stat-name">${pkmStatsData[3].stat.name}:</span>
-    <div class="progress-bar" id="spclAtkBar-${i}">0%</div>
-  </div>
-  <div class="progress-row">
-    <span class="stat-name">${pkmStatsData[4].stat.name}:</span>
-    <div class="progress-bar" id="spclDfnsBar-${i}">0%</div>
-  </div>
-  <div class="progress-row">
-    <span class="stat-name">${pkmStatsData[5].stat.name}:</span>
-    <div class="progress-bar" id="speedBar-${i}">0%</div>
-  </div>
-</div>`
-  pokemonTypeColor(i, pkmData);
-  statsBar(i, pkmStatsData);
-  showArrows();
+        </div>
+  `;
 }
 
 function showArrows() {
   let arrows = document.getElementById('dialog');
-  arrows.innerHTML += `<div class="arrow-wrapper">
+  arrows.innerHTML += `<div id="arrows" class="arrow-wrapper">
          <img onclick ='previousPokemon()' id="left-arrow" class="arrow" src="./img/arrow left.png">
         <img onclick='nextPokemon()' id="right-arrow" class="arrow" src="./img/arrow right.png">
            </div>
-            </div>
-           `
+           `;
 }
+
+function createPokemonCardTemplate(index) {
+  return `
+        <div onclick="openDialogWindow(${index}, event)" class="card" id="card-${index}">
+            <div class="dialog-content"></div>
+        </div>
+    `;
+}
+
+function showNoResults() {
+  return `<p class="no-results">No Pokémon found matching</p>`;
+}
+
